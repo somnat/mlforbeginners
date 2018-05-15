6cd0633e9d409836804d91f45be15cbc83e28273
roy <somnathroy86@gmail.com>
2018-05-15 22:49:59 +0530

    init

--- __init__.py
+++ __init__.py
@@ -1,2 +1,5 @@
 from linear_regression import LinearRegressionClassifier # noqa:F401
 from pca import PCA #noqa:F401
+from neural_network import Perceptron #noqa:F401 
+from utils import Activation
+from utils import LossFunction
---
+++ logistic_regression/logistic_regression.py
@@ -0,0 +1,6 @@
+import torch as ch
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
